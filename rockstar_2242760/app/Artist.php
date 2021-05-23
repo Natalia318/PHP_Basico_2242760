b4c35fa9d93437d8371d4c60e033b1731416c81a
Natalia Castro <ncastro212@misena.edu.co>
2021-05-23 18:29:54 -0500

    configurar BD, crear primer modelo

---
+++ rockstar_2242760/app/Artist.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Artist extends Model
+{
+    //vincular modelo a tabla
+    protected $table="Artist";
+
+    protected $primaryKey ="ArtistId";
+    public $timestamps = false;
+}
